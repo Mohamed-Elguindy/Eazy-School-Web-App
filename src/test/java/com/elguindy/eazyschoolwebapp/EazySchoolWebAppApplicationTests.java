eeb03beecba4751060e6d3a4f7f1e13ffbcf4cd8
Mohamed-Elguindy <mohamedwelguindy@gmail.com>
2025-09-23 15:30:02 +0300

    welcome page

---
+++ src/test/java/com/elguindy/eazyschoolwebapp/EazySchoolWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elguindy.eazyschoolwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazySchoolWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
