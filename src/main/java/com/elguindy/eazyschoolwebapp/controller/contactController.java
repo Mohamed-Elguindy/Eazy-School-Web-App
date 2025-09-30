afb6afa2752034c60289ff13fed36d258f66bc5e
Mohamed-Elguindy <mohamedwelguindy@gmail.com>
2025-09-30 11:19:21 +0300

    added Spring Security

--- src/main/java/com/elguindy/eazyschoolwebapp/controller/contactController.java
+++ src/main/java/com/elguindy/eazyschoolwebapp/controller/contactController.java
@@ -1,34 +1,41 @@
 package com.elguindy.eazyschoolwebapp.controller;
 
-import com.elguindy.eazyschoolwebapp.model.contact;
-import com.elguindy.eazyschoolwebapp.service.contactService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.elguindy.eazyschoolwebapp.model.Contact;
+import com.elguindy.eazyschoolwebapp.service.ContactService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
-
+@Slf4j
 @Controller
 public class contactController {
 
     @Autowired
-    private contactService contactService;
+    private ContactService contactService;
 
     @RequestMapping("/contact")
-    public String showContactPage() {
+    public String showContactPage(Model model) {
+        model.addAttribute("contact",new Contact());
         return "contact.html";
     }
 
     @PostMapping(value = "/saveMsg")
-    public ModelAndView saveMsg(contact contact)
+    public String saveMsg(@Valid @ModelAttribute("contact") Contact contact, Errors errors)
     {
+        if(errors.hasErrors()){
+            log.error("contact form has errors due to : "+errors.toString());
+            return "contact.html";
+        }
         contactService.saveMessage(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 
 }
--- src/main/java/com/elguindy/eazyschoolwebapp/service/ContactService.java
+++ src/main/java/com/elguindy/eazyschoolwebapp/service/ContactService.java
@@ -1,17 +1,17 @@
 package com.elguindy.eazyschoolwebapp.service;
 
 import com.elguindy.eazyschoolwebapp.controller.contactController;
-import com.elguindy.eazyschoolwebapp.model.contact;
+import com.elguindy.eazyschoolwebapp.model.Contact;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
-public class contactService {
+public class ContactService {
     private static Logger log = LoggerFactory.getLogger(contactController.class);
 
 
-    public boolean saveMessage(contact contact) {
+    public boolean saveMessage(Contact contact) {
         boolean isSaved = true;
         log.info(contact.toString());
         return isSaved;
